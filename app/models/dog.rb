e7742b9d463db3e544f149d1926cc92aa54fa881
Christian Thomas <christian.thomas.2894@gmail.com>
2018-08-07 14:52:48 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -5,11 +5,12 @@ class Dog
 
   def initialize(name, breed, age)
     @name = name
-    @breed = breed 
-    @age = age 
-  end 
+    @breed = breed
+    @age = age
+    
+  end
 
-  def self.all 
-    @@all 
-  end 
-end 
+  def self.all
+    @@all
+  end
+end
